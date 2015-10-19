9e3735baf98b5a66463811af8b90e4d153f29ea8
Charles Granet <cgranet@MacBook-Pro-de-Charles.local>
2015-10-19 11:12:08 +0200

    rename app

--- www/js/services.js
+++ www/js/services.js
@@ -1,26 +1,6 @@
 /*jslint browser: true*/
 /*global angular, cordova, StatusBar*/
 
-// var tastypieDataTransformer = function ($http) {
-//     return $http.defaults.transformResponse.concat([
-//         function (data, headersGetter) {
-//             return data.objects;
-//         }
-//     ])
-// };
-// 
-// var domain = "http://geoevent.herokuapp.com/api/v1/"
-// // var domain = "http://127.0.0.1:8000/api/v1/"
-// var api_key = "51a3dffafa923c080532d4fe8d1e670262941fbf"
-// var username = '33667045021'
-// 
-// var doLogin = function () {
-//     $http.post(domain+'auth/login/',
-//             {'username':'33667045021', 'password':'test'})
-//     .success(function (data, status, headers, config) {
-//     })
-// };
-// 
 angular.module('starter.services', [])
     .config(function ($provide, $tastypieProvider) {
         "use strict";
