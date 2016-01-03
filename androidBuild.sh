b27ebfaf3a125f0cd0a2daa44bd39c4a1dd9adad
charles <c.granet@gmail.com>
2016-01-03 18:34:56 +0100

    add angular-i18n in bower. json update android build script

--- androidBuild.sh
+++ androidBuild.sh
@@ -1,6 +1,6 @@
 rm woozup.apk
 cordova plugin rm cordova-plugin-console
 cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore varioware.keystore ./platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk varioware
-/home/charles/android-sdk-linux/build-tools/22.0.1/zipalign  -v 4 ./platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk woozup.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore varioware.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk varioware
+/home/charles/android-sdk-linux/build-tools/22.0.1/zipalign  -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk woozup.apk
 cordova plugin add cordova-plugin-console
