ebf39c815f7b0a25944b75c958bb9ee9d6b30d70
charles <c.granet@gmail.com>
2016-01-16 17:45:40 +0100

    modify env var (ionic build look for ANDROID_HOME...)

--- androidBuild.sh
+++ androidBuild.sh
@@ -2,5 +2,5 @@ rm woozup.apk
 cordova plugin rm cordova-plugin-console
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore varioware.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk varioware
-${ANDROID_SDK_ROOT}/build-tools/22.0.1/zipalign  -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk woozup.apk
+${ANDROID_HOME}/build-tools/22.0.1/zipalign  -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk woozup.apk
 cordova plugin add cordova-plugin-console
