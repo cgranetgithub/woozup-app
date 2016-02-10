53313924c5497bb445a863dd8f4768a92d3da64d
charles <charles@varioware.com>
2016-02-10 18:18:53 +0100

    change android icon (rounded corner)

--- androidBuild.sh
+++ androidBuild.sh
@@ -3,4 +3,3 @@ cordova plugin rm cordova-plugin-console
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore varioware.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk varioware
 ${ANDROID_HOME}/build-tools/22.0.1/zipalign  -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk woozup.apk
-cordova plugin add cordova-plugin-console
