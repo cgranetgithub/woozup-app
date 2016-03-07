215f42a41791617f47da4a0456e917bada51a156
Michael Bonfils (murlock42) <murlock42@gmail.com>
2016-03-07 22:36:18 +0100

    Override url.js with Prod Setting
    When build Android stuff
    (how to do for iOS build ?)

--- androidBuild.sh
+++ androidBuild.sh
@@ -1,5 +1,12 @@
 rm woozup.apk
+echo "Setting real URL"
+cat <<EOF >www/js/url.js
+var backend_url = "http://www.woozup.social/";
+EOF
 cordova plugin rm cordova-plugin-console
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore varioware.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk varioware
 ${ANDROID_HOME}/build-tools/22.0.1/zipalign  -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk woozup.apk
+
+echo "Reverting to default URL"
+git checkout www/js/url.js
