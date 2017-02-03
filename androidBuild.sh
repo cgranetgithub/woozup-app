c279940620a8c119f537b4f33250d44ae306d91e
charles <cgranet@protonmail.com>
2017-02-03 18:22:30 +0100

    version 1.1.9; fix ic notif icon (make a stronger build script)

--- androidBuild.sh
+++ androidBuild.sh
@@ -3,6 +3,10 @@ echo "Setting real URL"
 cat <<EOF >www/js/url.js
 var backend_url = "http://woozup.herokuapp.com/";
 EOF
+ionic platform remove android && ionic platform add android
+ionic resources android
+ionic platform update android
+cp -r resources/android/ic_notification/* platforms/android/res/
 cordova plugin rm cordova-plugin-console
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore varioware.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk varioware
